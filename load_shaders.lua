2040b2c2f34c5d067c1c1190d2963926df9c5b19
ProNoob135 <tavishmcewen@yahoo.com>
2018-08-26 01:25:57 -0700

    Moved shaders to folder

--- load_shaders.lua
+++ load_shaders.lua
@@ -1,14 +1,14 @@
-local shaderList = { {"mandelbrotShader.frag", "mandelbrot"}, {"iterationCheck.frag", "iterationCheck"} }
+local shaderList = love.filesystem.getDirectoryItems("shaders")
 local lastModified = {}
 local fileInfo = {}
 shader = {}
 
 function load_shaders()
     for i = 1, #shaderList do
-        fileInfo = love.filesystem.getInfo(shaderList[i][1])
+        fileInfo = love.filesystem.getInfo("shaders/" .. shaderList[i])
         if fileInfo.modtime ~= lastModified[i] then
-            local loadedShader = love.filesystem.read(shaderList[i][1])
-            shader[shaderList[i][2]] = love.graphics.newShader(loadedShader)
+            local loadedShader = love.filesystem.read("shaders/" .. shaderList[i])
+            shader[string.gsub( shaderList[i], ".frag", "")] = love.graphics.newShader(loadedShader)
             lastModified[i] = fileInfo.modtime
         end
     end
